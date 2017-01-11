0c037557b771f86ea555d49593911a8c53246f88
minamorl <minamorl@users.noreply.github.com>
2017-01-11 20:15:59 +0900

    Add unit tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,5 +5,37 @@ import { assert } from "chai"
 describe('op', () => {
   it('should replace original function', () => {
     assert.equal(app.echo instanceof OperationFunction, true)
+    assert.equal(app.showAppName instanceof OperationFunction, true)
+  })
+})
+describe('OperationFunction', () => {
+  it('stores original function as property', () => {
+    const original = function(arg: any) {}
+    const operation = new OperationFunction(original, "key")
+    assert.equal(operation.operation, original)
+  })
+  it('stores original function as property', () => {
+    const original = function(arg: any) {}
+    const operation = new OperationFunction(original, "key")
+    assert.equal(operation.operation, original)
+  })
+  it('stores key as property', () => {
+    const original = function(arg: any) {}
+    const operation = new OperationFunction(original, "key")
+    assert.equal(operation.key, "key")
+  })
+  it('acts as callable and return value as {[operation]: arg}', () => {
+    const original = function(arg: any) {}
+    const operation = new OperationFunction(original, "key")
+    assert.deepEqual(operation("value"), {
+      "key": "value"
+    })
+  })
+  it('accepts empty argument and transform undefined to null when instance is called', () => {
+    const original = function(arg: any) {}
+    const operation = new OperationFunction(original, "key")
+    assert.deepEqual(operation(), {
+      "key": null
+    })
   })
 })
