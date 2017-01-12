29c338ded7c11a4b66a4214b07f6a9f765c45c41
minamorl <minamorl@users.noreply.github.com>
2017-01-13 00:04:40 +0900

    Add load<T> function for isomorphic loading

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as root from "window-or-global"
 export type OperationFunctions = {[key: string]: OperationFunction}
 export const operationFunctionSymbol = Symbol.for('OperationFunction')
 export class OperationFunction extends Function {
@@ -6,6 +7,10 @@ export class OperationFunction extends Function {
     public key: string
   ) {
     super()
+    this.operation = (args: any) => {
+      root[Symbol.for("loader")] = require
+      return operation(args)
+    }
     let fn = function(arg?: any) {
       let result = {}
       result[key] = arg ? arg : null
@@ -23,4 +28,12 @@ export const op = (target: any, propertyKey: string, descriptor: PropertyDescrip
   descriptor.value = target[operationFunctionSymbol][propertyKey] = new OperationFunction(descriptor.value, propertyKey)
   // Add static function accessing across client side
   return descriptor
+}
+
+export function load<T>(moduleName: string): T {
+  const loader = root[Symbol.for("loader")]
+  if (loader)
+    return loader(moduleName)
+  let x: any = new Proxy(function() { return x }, {get: () => x, set: () => x})
+  return x as any as T
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,9 @@
+import * as root from "window-or-global"
 import { app } from "./app"
-import { op, OperationFunction } from "../src/index"
+import { op, OperationFunction, load } from "../src/index"
 import { assert } from "chai"
-
+// import type definition only
+import * as _chai from "chai"
 describe('op', () => {
   it('should replace original function', () => {
     assert.equal(app.echo instanceof OperationFunction, true)
@@ -10,14 +12,10 @@ describe('op', () => {
 })
 describe('OperationFunction', () => {
   it('stores original function as property', () => {
-    const original = function(arg: any) {}
+    const original = function(arg: any) { return 1 }
     const operation = new OperationFunction(original, "key")
-    assert.equal(operation.operation, original)
-  })
-  it('stores original function as property', () => {
-    const original = function(arg: any) {}
-    const operation = new OperationFunction(original, "key")
-    assert.equal(operation.operation, original)
+    assert.equal(operation.operation(null), original(null))
+    root[Symbol.for("loader")] = undefined
   })
   it('stores key as property', () => {
     const original = function(arg: any) {}
@@ -37,5 +35,33 @@ describe('OperationFunction', () => {
     assert.deepEqual(operation(), {
       "key": null
     })
+  })
+})
+describe('load', () => {
+  it('should be strongly typed as original', () => {
+    const chai = load<typeof _chai>("chai")
+    chai.assert.equal
+  })
+  it('should be get anything in property', () => {
+    const sample = load<any>("sample")
+    assert.isFunction(
+      sample.sample.sample.sample.sample.sample.sample.sample
+    )
+  })
+  it('should be set anything in property', () => {
+    const sample = load<any>("sample")
+    sample.sample.sample.sample.sample.sample.sample.sample = "a"
+  })
+  it('should be callable in property', () => {
+    const sample = load<any>("sample")
+    assert.isFunction(
+      sample.sample.sample.sample.sample.sample.sample.sample("test")
+    )
+  })
+  it('should be require actual module when global[Symbol.for("module")] is set', () => {
+    root[Symbol.for("loader")] = require
+    const chai1 = load<any>("chai")
+    assert.isNotFunction(chai1.assert.broken)
+    root[Symbol.for("loader")] = undefined
   })
 })
