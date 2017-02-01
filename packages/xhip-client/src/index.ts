9424b2a2725e4176f393cca5c468171c447f096b
minamorl <minamorl@users.noreply.github.com>
2017-02-01 20:47:33 +0900

    Make websocket connection be explicit

--- packages/xhip-client/src/index.ts
+++ packages/xhip-client/src/index.ts
@@ -1,24 +1,13 @@
 import "whatwg-fetch"
 
+const ReconnectingWebSocket = require('reconnecting-websocket')
+
 const createResponse = (parsed: any) => Object.assign({}, ...Object.keys(parsed).map(k => parsed[k]))
 
 export class Client {
   socket: WebSocket
   subscriptions: any[] = []
   constructor(public uri: string, public opts: { ssl: boolean }) {
-    const prefix = this.opts.ssl ? 'wss' : 'ws'
-    this.socket = new WebSocket(prefix + "://" + new URL(this.uri).host + "/ws")
-    this.socket.onmessage = (ev) => {
-      const parsed = JSON.parse(ev.data)
-      const allExecutedOperations = Object.keys(parsed)
-      for (let x of this.subscriptions) {
-        const { target } = x
-        const { receiver } = x
-        if (target.some((y: any) => allExecutedOperations.indexOf(y.key) !== -1)) {
-          receiver(createResponse(parsed))
-        }
-      }
-    }
   }
   exec = (ops: Array<any>) => {
     const body = JSON.stringify({
@@ -52,6 +41,21 @@ export class Client {
       this.socket.send(JSON.stringify({
         'operations': ops
       }))
+    }
+  }
+  connect() {
+    const prefix = this.opts.ssl ? 'wss' : 'ws'
+    this.socket = new ReconnectingWebSocket(prefix + "://" + new URL(this.uri).host + "/ws")
+    this.socket.onmessage = (ev) => {
+      const parsed = JSON.parse(ev.data)
+      const allExecutedOperations = Object.keys(parsed)
+      for (let x of this.subscriptions) {
+        const { target } = x
+        const { receiver } = x
+        if (target.some((y: any) => allExecutedOperations.indexOf(y.key) !== -1)) {
+          receiver(createResponse(parsed))
+        }
+      }
     }
   }
 }
