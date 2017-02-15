f6649c0bbb4365d8642a681c616787a5a086b22d
Ryohei Ikegami <iofg2100@gmail.com>
2017-02-15 19:03:11 +0900

    Update xhip-client test

--- packages/xhip-client/test/index.test.ts
+++ packages/xhip-client/test/index.test.ts
@@ -3,11 +3,11 @@ import * as sinon from "sinon"
 import { assert } from "chai"
 import * as fetchMock from "fetch-mock"
 
-fetchMock.post("*", JSON.stringify({
-    a: {
-        b: "a"
-    }
-}))
+fetchMock.post("*", JSON.stringify(
+  [{
+    b: "a"
+  }]
+))
 
 global["URL"] = sinon.mock()
 global["WebSocket"] = sinon.mock()
@@ -15,13 +15,13 @@ global["WebSocket"] = sinon.mock()
 console.log(fetch)
 
 describe('Client', () => {
-    describe('exec', () => {
-        it('should creatable from test', async () => {
-            const client = new Client("http://www.example.com/", { ssl: false })
-            const result = await client.exec([{ a: "a" }])
-            assert.deepEqual(result, {
-                b: "a"
-            })
+  describe('exec', () => {
+    it('should creatable from test', async () => {
+        const client = new Client("http://www.example.com/", { ssl: false })
+        const result = await client.exec([{ a: "a" }])
+        assert.deepEqual(result, {
+        b: "a"
         })
     })
+  })
 })
